69bdfbcd33781d11115deed0c0445947402ff3e8
Pablo Martin <pablo@odkq.com>
2024-10-23 11:44:38 +0200

    Commit script

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# pomo
+pomo.py
+=======
+
 Simple pomodoro timer for i3status (i3 and sway window managers)
+
+Configuration, in ~/.config/i3status/config:
+
+    order += "read_file p"
+
+Below:
+
+    read_file p {
+        path = "/var/tmp/pomo.status"
+    }
+
+
+Installation
+------------
+
+Put the script in a PATH accessible directory, as ~/.local/bin You may
+need to set the execution bit (chmod) on it, and rename it to 'pomo' for
+convenience
+
+Runtime dependencies
+--------------------
+
+- [zenity](https://gitlab.gnome.org/GNOME/zenity), for the modal dialog
+  signaling the end of the period.
+- [inotify_simple](https://github.com/chrisjbillington/inotify_simple) to
+    watch for changes in the input file
+
+Usage
+-----
+
+
+First the process needs to be running, either on the foreground,
+running pomo without parameters, or as a systemd service.
+
+When passed a parameter, it starts a new timer. For example
+
+    pomo.py 25          # Start a new pomodoro timer of 25 min
+
+Install as a local systemd service
+----------------------------------
+
+To run it with systemd as a user, write in
+~/.config/systemd/user/pomo.service:
+
+    [Unit]
+    Description=Pomo User Serivce
+
+    [Service]
+    Type=simple
+    ExecStart=--- home dir ---/.local/bin/pomo
+    Restart=on-failure
+    StandardOutput=file:%h/log_file
+
+    [Install]
+    WantedBy=default.target
+
+And do
+
+    $ systemctl --user enable pomo
+    $ systemctl --user start pomo
---
+++ pomo.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+"""
+A simple pomodoro timer to use with i3status for i3 or sway window managers.
+https://github.com/odkq/pomo
+
+Copyright (C) 2024 by Pablo Martin <pablo@odkq.com>. See LICENSE file.
+"""
+from asyncio import set_event_loop, new_event_loop, sleep as asleep
+from inotify_simple import INotify, flags
+from os.path import exists
+from subprocess import call
+from sys import argv, exit
+from time import time
+
+loop_period = 1
+period = 0
+inotify = None
+start = None
+status_path = "/var/tmp/pomo.status"
+period_path = "/var/tmp/pomo.period"
+expired = False
+
+
+def reload_period():
+    """Reload period (in minutes) from /var/tmp/pomo.period and start timer"""
+    global period, start, expired
+    try:
+        line = open(period_path, "r").readlines()[0]
+    except IndexError:
+        minutes = 0
+    try:
+        minutes = int(line)
+    except ValueError:
+        minutes = 0
+    period = minutes * 60
+    if period == 0:
+        update_tray("   ")
+    start = round(time())
+    expired = False
+
+
+def update_tray(s):
+    """Update the status path, which will be printed with i3status"""
+    try:
+        current = open(status_path, "r").readlines()[0]
+    except IndexError:  # file is empty
+        current = ""
+    if len(current) > 0 and current[-1] == "\n":
+        current = current[:-1]
+    if current == s:  # file contains the same string
+        return
+    print(f"Updating tray with {s}")
+    open(status_path, "w").write(s)
+
+
+def quantum():
+    """Update the countdown"""
+    global expired
+    for event in inotify.read(timeout=1):
+        reload_period()
+    if period == 0:
+        return
+    current = round(time())
+    delta = current - start
+    countdown = period - delta
+    print(f"quantum countdown {countdown} period {period} delta {delta}")
+    if countdown <= 0:
+        update_tray("   ")
+        if not expired:
+            call(["zenity", "--info",
+                  "--text=\"<span font='30'>⏳Finished ⏳</span>\"",
+                  "--width", "500", "--height", "500"])
+            expired = True
+        return
+    mins, secs = divmod(countdown, 60)
+    if mins > 0:
+        update_tray(f"⏳{mins}m{secs}s")
+    else:
+        update_tray(f"⏳{secs}s")
+
+
+async def run_periodically():
+    """Periodic callback for asyncio"""
+    while True:
+        quantum()
+        await asleep(loop_period)
+
+
+if len(argv) == 2:
+    try:
+        min = int(argv[1])
+    except ValueError:
+        print("Parameter must be a number (minutes), for example: pomo 25")
+        exit(1)
+    open(period_path, 'w').write(f"{min}")
+    exit(0)
+
+# Create the temporary files if not there
+for file in ["/var/tmp/pomo.status", "/var/tmp/pomo.period"]:
+    if not exists(file):
+        open(file, "w").write("")
+
+update_tray("   ")
+inotify = INotify()
+wd = inotify.add_watch(period_path, flags.MODIFY)
+loop = new_event_loop()
+set_event_loop(loop)
+loop.run_until_complete(run_periodically())
